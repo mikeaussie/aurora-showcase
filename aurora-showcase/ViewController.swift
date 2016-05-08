72adccf7f0171da6627c8f8e0dd29ec7546c573f
mikeaussie <michael.pyatin@me.com>
2016-05-08 16:31:54 +1000

    Initial Commit

---
+++ aurora-showcase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  aurora-showcase
+//
+//  Created by Mike Piatin on 8/05/2016.
+//  Copyright © 2016 Aurora Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
